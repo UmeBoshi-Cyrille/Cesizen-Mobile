f307bf4a30d2419fad26cfd228eca1152a72452b
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-03-27 17:33:08 +0100

    Add project files.

---
+++ Cesizen-Mobile/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+  ],
+};
