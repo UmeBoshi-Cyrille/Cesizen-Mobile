f55891a7167e602eaf883a322e09a5d244598d0a
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-04-13 23:26:37 +0200

    chore(config): capacitor installation

---
+++ Cesizen-Mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cesizen.mobile.app',
+  appName: 'cesizen-mobile',
+  webDir: 'dist/cesizen-mobile/browser'
+};
+
+export default config;
