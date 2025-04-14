53b5221cdba57fd633cb8a6c406a25de2dea0bce
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-04-14 18:45:05 +0200

    chore(etst): config karma

--- Cesizen-Mobile/src/app/app-navigation/app-navigation.component.spec.ts
+++ Cesizen-Mobile/src/app/app-navigation/app-navigation.component.spec.ts
@@ -1,36 +1,27 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppNavigationComponent } from './app-navigation.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('AppNavigationComponent', () => {
   let component: AppNavigationComponent;
   let fixture: ComponentFixture<AppNavigationComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppNavigationComponent],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       imports: [
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
+        AppNavigationComponent // Import standalone component directly
+      ],
+      providers: [
+        provideAnimations() // Required for Angular Material animations
       ]
-    });
-  }));
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AppNavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should compile', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
