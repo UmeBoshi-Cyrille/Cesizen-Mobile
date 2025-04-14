8c3247d6bc7e52d331f723ca9d634ea7f1f1486c
Umeboshi Cyrille <umehara_cyrille@outlook.com>
2025-04-14 15:55:11 +0200

    chore(config): added angular material package

--- Cesizen-Mobile/capacitor.config.ts
+++ Cesizen-Mobile/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cesizen.mobile.app',
   appName: 'cesizen-mobile',
-  webDir: 'dist/cesizen-mobile/browser'
+  webDir: 'dist/cesizen-mobile/browser',
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'https'
+  }
 };
 
 export default config;
